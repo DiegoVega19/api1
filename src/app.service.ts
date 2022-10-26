de6e4ef6f96fcf78ec1805041b2276a4497034f4
DiegoVega19 <davila9923@gmail.com>
2022-10-26 15:50:12 -0600

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getOther(): string {
+    return 'It works';
+  }
+}
